6376195dbbe0cffb3bdd4846fa5ab370a790a36d
Harmanpreet Gill <hpsgill95@gmail.com>
2019-01-18 16:33:21 -0800

    Week 2

---
+++ tweetme/src/tweets/views.py
@@ -0,0 +1,113 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse_lazy
+from django.views.generic import (
+                        CreateView,
+                        DetailView,
+                        DeleteView,
+                        ListView,
+                        UpdateView
+                        )
+
+from .forms import TweetModelForm
+from .mixins import FormUserNeededMixin, UserOwnerMixin
+from .models import Tweet
+
+
+
+# Create your views here.
+
+# Create
+
+class TweetCreateView(FormUserNeededMixin, CreateView):
+    form_class = TweetModelForm
+    template_name = 'tweets/create_view.html'
+    # success_url = reverse_lazy("tweet:detail")
+    # login_url = '/admin/'
+
+
+class TweetUpdateView(LoginRequiredMixin, UserOwnerMixin, UpdateView):
+    queryset = Tweet.objects.all()
+    form_class = TweetModelForm
+    template_name = 'tweets/update_view.html'
+    # success_url = "/tweet/"
+
+
+
+class TweetDeleteView(LoginRequiredMixin, DeleteView):
+    model = Tweet
+    template_name = 'tweets/delete_confirm.html'
+    success_url = reverse_lazy("tweet:list")
+
+
+     
+
+
+# def create_tweet_view(request):
+#     form = TweetModelForm(request.POST or None)
+#     if form.is_valid():
+#         instance = form.save(commit=False)
+#         instance.user = request.user
+#         instance.save()
+#     context = {
+#         "form":form
+#     }
+#     return render(request, 'tweets/create_view.html', context)
+
+#Retrieve
+
+
+class TweetDetailView(DetailView):
+    queryset = Tweet.objects.all()
+
+    # template_name = "tweets/detail_view.html"
+
+#     def get_object(self):
+#         print(self.kwargs)
+#         pk = self.kwargs.get("pk")
+#         obj = get_object_or_404(Tweet, pk = pk)
+#         return obj
+
+
+class TweetListView(ListView):
+    
+
+    def get_queryset(self, *args, **kwargs):
+        qs = Tweet.objects.all()
+        print(self.request.GET)
+        query = self.request.GET.get("q", None)
+        if query is not None:
+            qs = qs.filter(
+                    Q(content__icontains=query) |
+                    Q(user__username__icontains=query)
+                    )
+        return qs
+
+    def get_context_data(self, *args,  **kwargs):
+        context = super(TweetListView, self).get_context_data(*args, **kwargs)
+        # context ["another list "] = Tweet.objects.all()
+        # print(context)
+        return context
+
+
+def tweet_detail_view(request, id=1):
+    # obj = Tweet.objects.get(id=id) #GET from database
+    obj = get_object_or_404(Tweet, pk = pk)
+    print(obj)
+    context = {
+        "object" : obj
+    }
+    return render(request, "tweets/detail_view.html", context)
+
+
+# def tweet_list_view(request):
+#     queryset = Tweet.objects.all()
+#     print(queryset)
+#     for obj in queryset:
+#         print(obj.content )
+#     context = {
+#         "object_list" : queryset
+#     }
+#     return render(request, "tweets/list_view.html", context)
+
